81bcbe340bce2c02ba3b1810bee947b84a168c14
GuilhermeEDS <102765171+GuilhermeEDS@users.noreply.github.com>
2023-06-24 13:17:53 -0300

    modificações para receber os dados do back

--- src/Pages/Capitulos.js
+++ src/Pages/Capitulos.js
@@ -1,31 +1,53 @@
-import '../css/capitulos.css'
+import React, { useState, useEffect } from 'react';
+
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 
-import logoCs from '../Imgs/CS-IEEE UFRN Symbol.png';
-import logoPels from '../Imgs/Logo PELS_IAS UFRN.png';
-import logoWie from '../Imgs/LOGO WIE FUNDO TRANSPARENTE.png';
-import logoInClass from '../Imgs/Logo_In_Class.png';
- 
-function Capitulos() {
-    const lista = [{id: 0, nome: 'CS', imagem: logoCs}, {id: 1, nome: 'PELS', imagem: logoPels}, {id: 2, nome: 'Wie', imagem: logoWie}, {id: 3, nome: 'In Class', imagem: logoInClass}, {id: 4, nome: 'CS', imagem: logoCs}]
+import '../css/capitulos.css';
 
-    let capitulos = [];
-    lista.forEach(element => {
-        capitulos.push(<Col key={element.id}><a style={{textDecoration : 'none', color : 'black'}} href='/'>
-            <Image style={{marginBottom: 10}} title={element.nome} roundedCircle width={160} height={160} 
-            src={element.imagem} className='shadow-lg'></Image>
-        <h2 style={{textAlign: 'text-center'}}>{element.nome}</h2></a>
-        </Col>)
-    });
+function Capitulos() {
+    const [lista, setLista] = useState([]);
+
+    useEffect(() => {
+        const carregarCapitulos = async () => {
+            let data = await fetch("http://localhost:8080/capitulo/")
+            .then((response) => response.json())
+            .then((responseJSON) => setLista(responseJSON))
+            .catch(error => console.error(error) );
+            console.log(data)
+        }
+
+        carregarCapitulos();
+    }, []);
+
+    useEffect(()=>{console.log('lista:',lista)}, [lista]);
 
   return (
     <Container className='caps'>
         <h1 style={{textAlign: 'left', textDecoration: 'underline'}}>Capítulos</h1>
         <Row>
-            {capitulos}
+            {
+                lista.map( (element) => (
+                    <Col key={element.id}>
+                        <a style={{textDecoration : 'none', color : 'black'}} href='/'>
+                            <Image 
+                                style={{marginBottom: 10}} 
+                                title={element.nome} 
+                                roundedCircle 
+                                width={160} 
+                                height={160} 
+                                src={element.logo} 
+                                className='shadow-lg'
+                            />
+                            <h2 style={{textAlign: 'text-center'}}>
+                                {element.nome}
+                            </h2>
+                        </a>
+                    </Col>
+                ) )
+            }
         </Row>
     </Container>
   );
